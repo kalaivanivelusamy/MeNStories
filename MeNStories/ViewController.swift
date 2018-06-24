c7e3b4637d6a40a6e8932e021451baaa480a9369
Kalaivani Velusamy <kalai.v.vani@gmail.com>
2018-06-24 20:42:34 +0530

    added text

--- MeNStories/ViewController.swift
+++ MeNStories/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let type:String="type"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
