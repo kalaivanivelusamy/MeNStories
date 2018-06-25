c2774e0086225656b3d91a189e174305e8e42653
Kalaivani Velusamy <kalai.v.vani@gmail.com>
2018-06-25 11:38:20 +0530

    added location coordinate for initial loading of map Hard coded user's location added new custom annotation file

---
+++ MeNStories/CustomAnnotations.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAnnotations.swift
+//  MeNStories
+//
+//  Created by Kalaivani, Velusamy (623-Extern) on 25/06/18.
+//  Copyright © 2018 Kalaivani, Velusamy (623-Extern). All rights reserved.
+//
+
+import Foundation
--- MeNStories/ViewController.swift
+++ MeNStories/ViewController.swift
@@ -11,16 +11,23 @@ import MapKit
 class ViewController: UIViewController {
 
     let locationManager = CLLocationManager()
+    let initialLocation=CLLocation(latitude: 12.9804141, longitude: 77.7023445)
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkLocationAuthorizationStatus()
 
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+        centerUserLocationOnMap(location: initialLocation)
+    }
+    
     func checkLocationAuthorizationStatus() {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-           // mapView.showsUserLocation = true
+           mapView.showsUserLocation = true
         } else {
             locationManager.requestWhenInUseAuthorization()
         }
@@ -31,6 +38,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //helper methods
+    func centerUserLocationOnMap(location:CLLocation){
+        let regionDistance:CLLocationDistance = 500
+        let coordinateRegion=MKCoordinateRegionMakeWithDistance(location.coordinate,regionDistance, regionDistance)
+        mapView.setRegion(coordinateRegion, animated: true)
+        mapView.addAnnotation(<#T##annotation: MKAnnotation##MKAnnotation#>)
+
+    }
+    
 
 
 }
