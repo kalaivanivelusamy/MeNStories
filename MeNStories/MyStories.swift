e73ff67dcc93db13f5556ca29c9e6d73871dd85f
Kalaivani Velusamy <kalai.v.vani@gmail.com>
2018-07-04 23:50:33 +0530

    added stories file

---
+++ MeNStories/MyStories.swift
@@ -0,0 +1,10 @@
+//
+//  MyStories.swift
+//  MeNStories
+//
+//  Created by Kalaivani, Velusamy (623-Extern) on 04/07/18.
+//  Copyright © 2018 Kalaivani, Velusamy (623-Extern). All rights reserved.
+//
+
+import Foundation
+import Alamofire
--- MeNStories/ViewController.swift
+++ MeNStories/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import Alamofire
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapViewTrailingC: NSLayoutConstraint!
